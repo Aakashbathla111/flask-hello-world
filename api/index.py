a7298d3eb2c71f424c5bc77688d4fff51c1ffdd4
Aakashbathla111 <aakashbathla50@gmail.com>
2023-12-22 13:40:36 +0530

    pushing route

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/run_script', methods=['POST'])
+def run_script():
+    return jsonify({'output': "success"}), 200
+    
 
 @app.route('/about')
 def about():
